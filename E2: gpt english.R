05f0dcaf6c906dc79e0a7437066f8b7a5998d8d2
LeoZhang20 <82401700+LeoZhang20@users.noreply.github.com>
2024-05-31 19:56:14 +0200

    Update and rename gpt english.R to E2: gpt english.R

--- E2: gpt english.R
+++ E2: gpt english.R
@@ -23,6 +23,6 @@ prompts_df <- data.frame(
 gpt_english = rgpt(prompt_role_var = prompts_df$prompts_roles
                    , prompt_content_var = prompts_df$prompt
                    , id_var = prompts_df$prompt_id
-                   , param_max_tokens = 50
+                   , param_max_tokens = 30
                    , param_n = 5
                    , param_temperature = 0.99)
