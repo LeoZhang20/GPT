3a17cee4880a1c28618d5c1fc081433f30d2653d
LeoZhang20 <82401700+LeoZhang20@users.noreply.github.com>
2024-06-02 22:19:10 +0200

    Update E2. keywords matching in English.R

--- E2. keywords matching in English.R
+++ E2. keywords matching in English.R
@@ -6,26 +6,27 @@ completion <- data.frame(
 
 # Create keywords
 categories <- data.frame(
-  key_word = c("Soong Chu-yu", "James Soong", "People First Party", 
-               "Han Kuo-yu", "Kuomintang", "Nationalist Party", "Chinese Nationalist Party", "KMT", 
-               "Tsai Ing-wen", "Democratic Progressive Party", "DPP",
-               "invalid vote", 
-               "did not vote", "Unlikely"),
-  gpt_content = c("Soong Chu-yu", "Soong Chu-yu", "Soong Chu-yu", 
-                  "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu",
-                  "Tsai Ing-wen", "Tsai Ing-wen", "Tsai Ing-wen",
-                  "invalid vote", 
-                  "did not vote", "did not vote")
+  key_word = c("Soong Chu-yu", "James Soong", "People First Party", "Sandra Yu", "PFP", 
+               "Han Kuo-yu", "Kuomintang", "Nationalist Party", "Chinese Nationalist Party", "KMT", "Chang San-cheng", 
+               "Tsai Ing-wen", "Democratic Progressive Party", "DPP", "Lai Ching-te", "Centre to centre-left political party", 
+               "invalid vote", "did not vote", "Unlikely", "did not vote", "didn't vote"),
+  gpt_content = c("Soong Chu-yu", "Soong Chu-yu", "Soong Chu-yu", "Soong Chu-yu", "Soong Chu-yu", 
+                  "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu", "Han Kuo-yu",
+                  "Tsai Ing-wen", "Tsai Ing-wen", "Tsai Ing-wen", "Tsai Ing-wen", "Tsai Ing-wen", 
+                  "<NA>", "<NA>", "<NA>", "<NA>", "<NA>")
 )
 
 # find out key words
 find_category <- function(gpt_content, categories) {
-  for (i in 1:nrow(categories)) {
-    if (grepl(categories$key_word[i], gpt_content, fixed = TRUE)) {
-      return(categories$gpt_content[i])
-    }
+  matches <- sapply(categories$key_word, function(keyword) grepl(keyword, gpt_content, fixed = TRUE))
+  matched_categories <- categories$gpt_content[matches]
+  if (sum(matches) >= 2 && length(unique(matched_categories)) > 1) {
+    return(NA)
+  } else if (length(matched_categories) >= 1) {
+    return(matched_categories[1])
+  } else {
+    return(NA)
   }
-  return(NA)
 }
 
 # match every data
