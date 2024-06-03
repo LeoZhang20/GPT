6e1b646224d4fe3fff1b87e1463a287378bd3141
LeoZhang20 <82401700+LeoZhang20@users.noreply.github.com>
2024-06-03 19:14:30 +0200

    Update T3: taiwan gpt.R

--- T3: taiwan gpt.R
+++ T3: taiwan gpt.R
@@ -5,7 +5,7 @@ rgpt_authenticate("H:/Bachelorarbeit/access_key1.txt")
 taiwan_prompts <- data.frame(
   prompts_roles = rep("user", 1472), 
   prompt_id = complete_dataT$id,
-  prompt = taiwan_question)
+  prompt = taiwan_result_clean)
 
 gpt_taiwan = rgpt(prompt_role_var = taiwan_prompts$prompts_roles
                     , prompt_content_var = taiwan_prompts$prompt
